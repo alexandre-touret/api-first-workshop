2de9b195b4dfd7de46f87248a54ca68cb3084d0c
Alexandre Touret <alexandre-touret@users.noreply.github.com>
2024-12-18 09:51:33 +0100

    chore: Remove class after merge
    chore: Fix Merge

--- src/main/java/info/touret/guitarheaven/domain/model/Guitar.java
+++ src/main/java/info/touret/guitarheaven/domain/model/Guitar.java
@@ -2,6 +2,6 @@ package info.touret.guitarheaven.domain.model;
 
 public record Guitar(Long id, String name, TYPE type, Double price, int stock) {
     public enum TYPE {
-        ELECTRIC, CLASSIC, FOLK, GIPSY
+        ELECTRIC, CLASSIC, FOLK, GIPSY,JAZZ
     }
 }
