12ee2e63d7eab905666b8bf9baa5426315998e41
Alexandre Touret <alexandre-touret@users.noreply.github.com>
2024-12-22 00:32:01 +0100

    feat: Fix issue in OrderServiceTest.java

--- src/main/java/info/touret/guitarheaven/domain/service/GuitarService.java
+++ src/main/java/info/touret/guitarheaven/domain/service/GuitarService.java
@@ -13,7 +13,7 @@ public class GuitarService {
     private final GuitarPort guitarPort;
 
     /**
-     * @param guitarPort
+     * @param guitarPort the Port for reaching the infrastructure
      */
     public GuitarService(GuitarPort guitarPort) {
         this.guitarPort = guitarPort;
@@ -28,14 +28,21 @@ public class GuitarService {
         return guitarPort.listAll();
     }
 
+    /**
+     * Finds all the guitars matching the IDs
+     *
+     * @param ids list of Guitar IDs
+     * @return The list of guitars. It will be empty if no guitar is found
+     */
     public List<Guitar> findGuitarsByIds(List<Long> ids) {
+        System.err.println(">>>> FIND BY IDS {}" + ids);
         return guitarPort.findGuitarsByIds(ids);
     }
 
     /**
      * Creates a guitar
      *
-     * @param guitar
+     * @param guitar the Guitar to create. The ID must not be filled
      */
     public void createGuitar(Guitar guitar) {
         guitarPort.save(guitar);
@@ -44,7 +51,7 @@ public class GuitarService {
     /**
      * Updates the guitar
      *
-     * @param guitar
+     * @param guitar the guitar to update
      * @return The updated guitar
      */
     public Guitar updateGuitar(Guitar guitar) {
@@ -54,8 +61,8 @@ public class GuitarService {
     /**
      * Deletes one guitar
      *
-     * @param guitarId
-     * @return
+     * @param guitarId : The ID of the guitar to delete
+     * @return true if the guitar is deleted. False instead.
      */
     public boolean deleteGuitar(Long guitarId) {
         return guitarPort.delete(guitarId);
