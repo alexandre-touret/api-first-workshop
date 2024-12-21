8c5ef4ddd6f41f63e476fa81b162077c749a0e05
Alexandre Touret <alexandre-touret@users.noreply.github.com>
2024-12-21 11:00:40 +0100

    chore: Add documentation + Onion Architecture Test for the EBAY Adapter

--- src/main/java/info/touret/guitarheaven/domain/service/SupplierCatalogPort.java
+++ src/main/java/info/touret/guitarheaven/domain/service/SupplierCatalogPort.java
@@ -1,9 +1,15 @@
 package info.touret.guitarheaven.domain.service;
 
-import java.util.Optional;
 import java.util.OptionalDouble;
 
+/**
+ * Specifies the interactions with the supplier catalog
+ */
 public interface SupplierCatalogPort {
 
+    /**
+     * @param guitarName The name of the guitar to be searched
+     * @return The average price if corresponding guitars exist, <code>OptionalDouble.empty()</code>
+     */
     OptionalDouble getAverageGuitarPrice(String guitarName);
 }
