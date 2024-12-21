de7c9e137f11c9be2d2721f5972ffa43bf78e006
Alexandre Touret <alexandre-touret@users.noreply.github.com>
2024-12-21 11:00:40 +0100

    chore: Add documentation + Onion Architecture Test for the EBAY Adapter

--- src/main/java/info/touret/guitarheaven/infrastructure/ebay/EbayDiscounterAdapter.java
+++ src/main/java/info/touret/guitarheaven/infrastructure/ebay/EbayDiscounterAdapter.java
@@ -3,10 +3,8 @@ package info.touret.guitarheaven.infrastructure.ebay;
 
 import info.touret.guitarheaven.domain.service.SupplierCatalogPort;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
-import java.util.Optional;
 import java.util.OptionalDouble;
 
 @ApplicationScoped
@@ -14,9 +12,6 @@ public class EbayDiscounterAdapter implements SupplierCatalogPort {
 
     @RestClient
     private EbayClient ebayClient;
-
-    public EbayDiscounterAdapter() {
-    }
 
     @Override
     public OptionalDouble getAverageGuitarPrice(String guitarName) {
