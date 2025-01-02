dc7d8ce58b93d8884d4cf41e0ec864c5df90bebb
Alexandre Touret <alexandre-touret@users.noreply.github.com>
2025-01-02 12:17:36 +0100

    fix(TO PICK): ArchitectureTest upgrade with org.slf4j as an acceptable dependency for the domain Fix import SQL removing the totalPrice field
    (cherry picked from commit f58a4e880457437ec577bacfb1e673ae14a86e51)

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -14,10 +14,10 @@ values (1001, '628766d4-fee3-46dd-8bcb-426cffb4d684'::UUID, 'Gibson Les Paul Sta
 
 
 -- insert into GuitarOrder (discountRequested, totalPrice, createdAt, id, orderId) values (100, 2500, '2004-10-19 10:23:54+02', 999, gen_random_uuid());
-insert into GuitarOrder (discountRequested, totalPrice, createdAt, id, orderId)
-values (100, 2500, '2004-10-19 10:23:54+02', 999, '292a485f-a56a-4938-8f1a-bbbbbbbbbbb1'::UUID);
-insert into GuitarOrder (discountRequested, totalPrice, createdAt, id, orderId)
-values (100, 2500, '2004-10-19 10:23:54+02', 1001, '292a485f-a56a-4938-8f1a-bbbbbbbbbba1'::UUID);
+insert into GuitarOrder (discountRequested,  createdAt, id, orderId)
+values (100,  '2004-10-19 10:23:54+02', 999, '292a485f-a56a-4938-8f1a-bbbbbbbbbbb1'::UUID);
+insert into GuitarOrder (discountRequested,  createdAt, id, orderId)
+values (100,  '2004-10-19 10:23:54+02', 1001, '292a485f-a56a-4938-8f1a-bbbbbbbbbba1'::UUID);
 
 insert into Guitar_GuitarOrder(orders_id, guitars_id)
 values (999, 999);
