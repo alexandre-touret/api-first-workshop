c0801f1ce56c60f07a7a1205e22a7d1376019935
Alexandre Touret <alexandre-touret@users.noreply.github.com>
2025-01-03 13:31:21 +0100

    chore(TO PICK): Fix problem details http response & improve unit / integration tests

--- src/test/java/info/touret/guitarheaven/test/application/QuoteResourceTest.java
+++ src/test/java/info/touret/guitarheaven/test/application/QuoteResourceTest.java
@@ -3,17 +3,23 @@ package info.touret.guitarheaven.test.application;
 import info.touret.guitarheaven.application.generated.model.QuoteDto;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.hamcrest.core.Is;
 import org.hamcrest.text.MatchesPattern;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @QuarkusTest
 public class QuoteResourceTest {
     private static final String UUID_REGEX = "[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}";
 
+    @Order(2)
     @Test
     void should_get_a_list_successfully() {
         RestAssured.given()
@@ -23,7 +29,7 @@ public class QuoteResourceTest {
                 .assertThat().body("isEmpty()", Is.is(false));
     }
 
-
+    @Order(1)
     @Test
     void should_create_successfully() {
         var quote = new QuoteDto().quoteId(null).orderId(UUID.fromString("292a485f-a56a-4938-8f1a-bbbbbbbbbbc1")).discountInUSD(10D).totalPriceWithDiscountInUSD(null).createdAt(OffsetDateTime.now());
@@ -39,6 +45,7 @@ public class QuoteResourceTest {
 
     }
 
+    @Order(3)
     @Test
     void should_create_and_fail() {
         var quote = new QuoteDto().quoteId(null).orderId(UUID.fromString("292a485f-a56a-4938-8f1a-bbbbbbbbbbb9")).discountInUSD(10D).totalPriceWithDiscountInUSD(null).createdAt(OffsetDateTime.now());
@@ -49,6 +56,6 @@ public class QuoteResourceTest {
                 .when()
                 .post("/quotes")
                 .then()
-                .statusCode(400);
+                .statusCode(417).contentType(ContentType.fromContentType("application/problem+json"));
     }
 }
--- src/test/java/info/touret/guitarheaven/test/domain/service/DiscountServiceTest.java
+++ src/test/java/info/touret/guitarheaven/test/domain/service/DiscountServiceTest.java
@@ -14,6 +14,7 @@ import java.util.OptionalDouble;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Mockito.when;
 
@@ -50,5 +51,10 @@ class DiscountServiceTest {
     void should_return_the_sum_of_the_list_successfully() {
         when(supplierCatalogPort.getAverageGuitarPrice(contains("Fender"))).thenReturn(OptionalDouble.of(900d));
         assertEquals(600d, discountService.getTotalDiscount(List.of(new Guitar(1L, UUID.randomUUID(), "Fender", Guitar.TYPE.ELECTRIC, 1000d, 10), new Guitar(2L, UUID.randomUUID(), "Fender", Guitar.TYPE.ELECTRIC, 2000d, 10))));
+    }
+
+    @Test
+    void should_throw_IAE() {
+        assertThrows(IllegalArgumentException.class, () -> discountService.getDiscountInUSD(new Guitar(1L, null, null, Guitar.TYPE.ELECTRIC, 100D, 10)));
     }
 }
